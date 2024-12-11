0b957506946124dafd7bc93fa7fd970859f377b2
Innermost <77298273+innermost47@users.noreply.github.com>
2024-12-11 16:19:55 +0100

    Update loginService.js
    check if possible registration

--- src/api/loginService.js
+++ src/api/loginService.js
@@ -3,4 +3,5 @@ import api from "./api";
 export const loginService = {
   login: async (credentials) => await api.post("/login", credentials),
   verify2FA: async (data) => await api.post("/verify-2fa", data),
+  checkRegistrationAvailability: async () => return await api.get('/check-registration-availability'),
 };
